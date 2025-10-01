164db86d3a8a19acc719ac7c7c346836691c6724
Gerard Toscano asto <gerardtoscanoasto@Mac-mini-de-Gerard.local>
2025-10-01 14:15:40 -0500

    fix

--- dist/interfaces/routes/checkout.js
+++ dist/interfaces/routes/checkout.js
@@ -63,6 +63,7 @@ checkoutRoutes.post('/create-checkout-session', async (c) => {
             line_items,
             metadata: deviceToken ? { fcmToken: deviceToken } : undefined
         });
+        console.log(`Received link: ${session.url}`);
         return c.json({ url: session.url });
     }
     catch (err) {
